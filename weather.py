9e9222834501e63672b1ac3461698862d7312824
Antonina Novikova <novikovantoninalex@gmail.com>
2025-01-30 16:35:57 +0400

    Delete debug print

--- weather.py
+++ weather.py
@@ -4,7 +4,6 @@ import requests
 def send_request(city):
     payload = {"n":"","T":"","q":"","M":"","lang": "ru"}  
     response = requests.get(f'https://wttr.in/{city}', params=payload) 
-    print(response.url)
     response.raise_for_status()  
     return response.text
 
