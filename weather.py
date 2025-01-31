8adb593c45392f5e7d4e148c756e83c8753a3d02
Antonina Novikova <novikovantoninalex@gmail.com>
2025-01-31 16:48:39 +0400

    rename function def

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def send_request(city):
+def request_weather(city):
     payload = {"n":"","T":"","q":"","M":"","lang": "ru"}  
     response = requests.get(f'https://wttr.in/{city}', params=payload) 
     response.raise_for_status()  
@@ -11,7 +11,7 @@ def main():
     cities = ('Лондон', 'Шереметьево', 'Череповец')  
     
     for city in cities:
-        weather_info = send_request(city)
+        weather_info = request_weather(city)
         print(f"Погода в {city}:\n{weather_info}\n")
 
 if __name__ == '__main__':
